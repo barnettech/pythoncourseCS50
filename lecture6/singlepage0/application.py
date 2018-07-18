70821249ff7ecb682ef99d5d41973af26f5d5c6b
James Barnett <barnettech@gmail.com>
2018-07-17 20:50:52 -0400

    adds lecture6 to repository

---
+++ lecture6/singlepage0/application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+texts = ["First first first first first first first first first",
+         "Second second second second second second second second second",
+         "Third third third third third third third third third"]
+
+@app.route("/first")
+def first():
+    return texts[0]
+
+@app.route("/second")
+def second():
+    return texts[1]
+
+@app.route("/third")
+def third():
+    return texts[2]
